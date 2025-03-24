80ebb4370f30aa45911cc40a5c582e9e6bd6970f
snehavarshney <snehavarshney111@gmail.com>
2025-03-24 13:42:37 +0530

    Added: Ability to launch BridgeLabz site in a new tab on clicking BridgeLabz Logo

--- BridgeLabz-Angular-Started/BridgeLabz-Angular-Started/src/app/app.component.ts
+++ BridgeLabz-Angular-Started/BridgeLabz-Angular-Started/src/app/app.component.ts
@@ -13,9 +13,20 @@ export class AppComponent {
   //UC2
   imgUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
 
+  //UC3
+  url = "https://www.bridgelabz.com";
+
+
   ngOnInit() : void {
     //UC1
     this.title = "Hello from BridgeLabz.";
+
+  }
+
+  onClick(event : Event){
+
+    console.log("Save button is clicked : ", event);
+    window.open(this.url, "_blank");
 
   }
 
