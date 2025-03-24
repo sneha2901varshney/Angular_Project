4b36b3580a73e9e162df3f70c1338b0fe93686de
snehavarshney <snehavarshney111@gmail.com>
2025-03-24 13:51:55 +0530

    Added: Display Something with a user input name using two way binding

--- BridgeLabz-Angular-Started/BridgeLabz-Angular-Started/src/app/app.component.ts
+++ BridgeLabz-Angular-Started/BridgeLabz-Angular-Started/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'Helloworld';
 
@@ -16,6 +18,8 @@ export class AppComponent {
   //UC3
   url = "https://www.bridgelabz.com";
 
+  //UC4
+  userName: string = "";
 
   ngOnInit() : void {
     //UC1
